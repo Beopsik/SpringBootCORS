1fbbfcfcc76701577b03f0316d4cd24fa8f2f1df
BeobSik <tjswl756@naver.com>
2021-08-17 10:58:53 +0900

    feat: add cors

--- src/main/java/com/example/springbootcors/SpringBootCorsApplication.java
+++ src/main/java/com/example/springbootcors/SpringBootCorsApplication.java
@@ -2,9 +2,18 @@ package com.example.springbootcors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootCorsApplication {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCorsApplication.class, args);
